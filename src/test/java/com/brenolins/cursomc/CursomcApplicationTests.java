d0789d899baede1fc18f48f10536c7c521b626fb
Breno Lins <brenoh21@gmail.com>
2020-05-12 11:26:55 -0300

    Projeto criado

---
+++ src/test/java/com/brenolins/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenolins.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
