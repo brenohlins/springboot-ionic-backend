f42eee92efb0ba5f436be3a70e27ee836c9e3d52
Breno Lins <brenoh21@gmail.com>
2020-05-13 10:18:54 -0300

    Operação de instânciação

--- src/main/java/com/brenolins/cursomc/CursomcApplication.java
+++ src/main/java/com/brenolins/cursomc/CursomcApplication.java
@@ -1,13 +1,29 @@
 package com.brenolins.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.brenolins.cursomc.domain.Categoria;
+import com.brenolins.cursomc.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Info");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
+
 	}
 
 }
