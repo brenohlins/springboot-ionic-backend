93067fb47e72aa145a4a791a0f61abe5cb4cdfca
Breno Lins <brenoh21@gmail.com>
2020-05-18 17:26:20 -0300

    Inserindo nova categoria

--- src/main/java/com/brenolins/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/brenolins/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.brenolins.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
 
 import com.brenolins.cursomc.domain.Categoria;
 import com.brenolins.cursomc.service.CategoriaService;
@@ -26,4 +31,12 @@ public class CategoriaResource {
 		Categoria obj = service.find(id);
 		return  ResponseEntity.ok(obj);
 	}
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri =  ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+	
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
