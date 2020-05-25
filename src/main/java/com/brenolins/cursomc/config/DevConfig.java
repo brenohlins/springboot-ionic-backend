f45b70979f20055dd6536fac2a5288b667e567ad
Breno Lins <brenoh21@gmail.com>
2020-05-25 09:36:30 -0300

    Envio para prod

--- src/main/java/com/brenolins/cursomc/config/DevConfig.java
+++ src/main/java/com/brenolins/cursomc/config/DevConfig.java
@@ -3,7 +3,6 @@ package com.brenolins.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -18,15 +17,11 @@ public class DevConfig {
 	@Autowired
 	private DBService dbService;
 	
-	@Value("$(spring.jpa.hibernate.ddl-auto)")
-	private String strategy;
 	
 	
 	@Bean
 	public boolean InstantiateDatabase() throws ParseException{
-		 if(!"create".equals(strategy)) {
-			 return false;
-		 }
+		
 		
 		dbService.InstantiateTestDatabase();
 		return true;
